80ccd60f3c3976d2586a4f31c6aac504c75a5798
Adheesh Bhatia <adheesh.bhatia@myntra.com>
2016-10-27 10:17:01 +0530

    init

---
+++ index.js
@@ -0,0 +1,36 @@
+var assertErr = require('assert-err')
+var GraphQLScalarType = require('graphql').GraphQLScalarType
+var GraphQLError = require('graphql/error').GraphQLError
+var Kind = require('graphql/language').Kind
+
+var parser = function (ast, validate) {
+  if (!validate) {
+    return ast.value
+  }
+
+  if(!options.validate(ast.value)) {
+    throw new GraphQLError(options.error, [ast]);
+  }
+
+  return ast.value;
+}
+
+module.exports = function (options) {
+  return new GraphQLScalarType({
+    name: options.name,
+    description: options.description,
+    serialize: function (value) {
+      return value
+    },
+    parseValue: function (value) {
+      var ast = {
+        kind: Kind.STRING,
+        value: value
+      }
+      return parser(ast, options)
+    },
+    parseLiteral: function (ast) {
+      return parser(ast, options)
+    }
+  }) 
+}
